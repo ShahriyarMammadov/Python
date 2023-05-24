f007f6e351ca12fe2dd241bb115648e9d71c0e73
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-05-24 22:33:16 +0400

    and or

--- stringMethods.py
+++ stringMethods.py
@@ -134,11 +134,121 @@
 
 # daxil edilen ededin kvarati ile kubunun cemini hesablayan funksiya
 
-a=int(input('ededi daxil edin '))
-def jk():
-    global a
-    s=0
-    kvadrat=a**2
-    kub=a**3
-    print(kvadrat+kub)
-jk()
+# a=int(input('ededi daxil edin '))
+# def jk():
+#     global a
+#     s=0
+#     kvadrat=a**2
+#     kub=a**3
+#     print(kvadrat + kub)
+# jk()
+
+# ----------------------------------------
+
+
+# daxil edilen araliq qeder cut ededleri teyin eded funksiya
+
+# a=int(input('ededi daxil edin:'))
+# def df(): 
+#     for i in range (1,a):
+#         if i%2 == 0:
+#             print(i)
+# df()
+
+
+# daxil edilen ededin sonuncu ededinden 1 vahid evvelde yerlesen ededi teyin eden funksiya
+
+# daxilEdilenEded = input("ededi daxil edin: ")
+
+# def daxilEdilenEdeddenEvvelkiEded():
+#     listeda = list(daxilEdilenEded)
+#     print(listeda[len(listeda) - 2])
+
+# daxilEdilenEdeddenEvvelkiEded()
+
+
+# daxil ededin ededin polindrom olub olmamasini yoxlayan funksiya
+
+# daxilEdilen = input("ededi daxil edin: ")
+
+# def polindromCheck():
+#     reverseEded = (str(daxilEdilen)[::-1])
+#     if (reverseEded == daxilEdilen):
+#         print("polindromdur")
+#     else:
+#         print("polindrom deyil")
+# polindromCheck()
+
+# daxil edilen menfi ededi musbet edede ceviren funksiya ve eded musbetdirse musbet ededin daxil edildiyini bildirmek
+
+# a= int(input('ededi daxil edin: '))
+
+# def gh():
+#     global a
+#     if (a < 0):
+#         a=a* (-1)
+#         print(a)
+# gh()
+
+
+# daxil edilen ededin daxil edilen edede qaliqsiz bolunub bolunmediyini teyin eded funksiya
+
+# a=int(input('ededi daxil edin:'))
+# b=int(input('ededi daxil edin:'))
+
+# def ugh():
+#     if (a%b == 0):
+#         print('a ededi b ededine bolunur')
+#     else:
+#         print('a ededi b ededine bolunmur')
+# ugh()  
+
+# daxil edilen 1den 7-ye qeder ededin heftenin hansi gunu oldugunu teyin e
+
+# a= int(input(' ededi yazin: '))
+# def alma():
+#     if (a == 1):
+#         print('bazar ertesi')
+#     elif (a == 2):
+#         print('cersembe axsami')
+#     else:
+#         print('gunleri duzgun daxil edin')
+# alma()    
+
+# -----------------------------
+
+# and, or
+
+# 1-den 100-e  qeder olan ededlerden 2-ye ve 40-ya qaliqsiz bolunen ededi teyin eden funksiya
+
+# and
+# def qaliqsiz_bolunen():
+#     for i in range(1,101):
+#         if(i % 2 == 0 and i % 40 == 0 ):
+#             print(i)
+
+# qaliqsiz_bolunen()
+
+# or
+
+# def qaliqsiz_bolunen():
+#     for i in range(1,101):
+#         if(i % 2 == 0 or i % 40 == 0):
+#             print(i)
+
+# qaliqsiz_bolunen()
+
+# 1-den 5000-e qeder olan ededlerden hem cut ededleri hemde 4500-den boyuk olan ededleri ekrana cixaran funksiya
+# def alma():
+#     for i in range (1,5001):
+#         if (i % 2 ==0 and i>=4500):
+#             print(i)
+# alma()            
+
+# 1-den 500-e qeder 200-e ve ya 400-e qaliqsiz bolunen ededleri teyin eden funksiya
+s=0
+def xoruz():
+    for i in range (1,501):
+        if (i % 200 == 0 or i % 400 == 0):
+            print(s) 
+xoruz()            
